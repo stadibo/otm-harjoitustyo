5c6b03851d0354e0f0a34caf156194c62cb1edcb
stadibo <jesperleonard@gmail.com>
2018-03-19 20:43:53 +0200

    gitignore update

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,50 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(100);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void korttiKontruktoriToimiiOikein() {
+        assertEquals("saldo: 1.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataSaldoaOikein() {
+        kortti.lataaRahaa(155);
+        assertEquals("saldo: 2.55", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiVaheneJosOttaaLiikaa() {
+        kortti.otaRahaa(150);
+        assertEquals("saldo: 1.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinKunRahaRiittaa() {
+        kortti.otaRahaa(70);
+        assertEquals("saldo: 0.30", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaFalseKunSaldoEiVahene() {
+        assertEquals(false, kortti.otaRahaa(150));
+    }
+    
+    @Test
+    public void palauttaaTrueKunSaldoRiittaa() {
+        assertEquals(true, kortti.otaRahaa(70));
+    }
+    
+    @Test
+    public void saldoPalauttaaOikeanSaldon() {
+        assertEquals(100, kortti.saldo());
+    }
+    
 }
